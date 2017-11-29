52042b8c5e0e0f89a0050c15297a6e5b05de2e1e
Andrew McBryde <andrewmcbryde100@gmail.com>
2017-11-29 04:12:33 -0500

    started lisp file

---
+++ Lisp/coll.lisp
@@ -0,0 +1,35 @@
+#!/usr/bin/sbcl --script
+; CSC330
+; Assignment 3 - Collatz Conjecture
+;
+; Author: Devin McBryde
+;
+;
+;
+
+(defvar maxValues)
+(setf maxValues(make-array (list 2 10)))
+
+(defvar minVal 0)
+
+(dotimes (i 10)
+	(setf (aref maxValues 0 i) 0)
+)
+
+(dotimes (i 10)
+
+	;while loop
+
+	;if
+
+		;for loop
+
+			;if
+
+		;for loop
+
+			;if
+
+)
+
+;print maxvalues
